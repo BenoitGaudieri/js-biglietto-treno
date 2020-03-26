b298e8a7b86d0231c185817c0db4c973b15c3f25
BenoitGaudieri <benalek@gmail.com>
2020-03-26 14:46:38 +0100

    feat. logic visualization on site

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,16 +3,19 @@
 // Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
 // Mostrare a schermo il prezzo del biglietto, indicando anche se è stato applicato un eventuale sconto.
 
+// Var declaration
 var priceToKm;
 var reduction;
 var age;
 var kmToTravel;
 var result;
 
+// Var assignment
 age = parseInt(prompt("How old are you?"));
 kmToTravel = parseInt(prompt("How many km?"));
 priceToKm = 0.21;
 
+// Logic
 if (age < 18) {
     reduction = 20 / 100;
 } else if (age > 65) {
@@ -23,8 +26,18 @@ if (reduction) {
     result = kmToTravel * priceToKm;
     result = result - result * reduction;
     console.log("reduction of " + reduction);
+    document.getElementById(
+        "js-price"
+    ).innerHTML = `You are going to pay ${result}€ for your travel.`;
+    document.getElementById(
+        "js-sale"
+    ).innerHTML = `A reduction of the ${reduction *
+        100}% was applied because you are ${age} years old!`;
 } else {
     result = kmToTravel * priceToKm;
+    document.getElementById(
+        "js-price"
+    ).innerHTML = `You are going to pay ${result}€ for your travel.`;
     console.log("no reduction");
 }
 
